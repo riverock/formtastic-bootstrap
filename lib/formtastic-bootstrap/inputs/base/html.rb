0de7d79b2fc8a4c66d076b107cf0c1174481ebc5
Nic Boie <nic@webvolta.com>
2025-10-29 17:10:49 -0500

    Deal with string keys from upstream

--- lib/formtastic-bootstrap/inputs/base/html.rb
+++ lib/formtastic-bootstrap/inputs/base/html.rb
@@ -2,25 +2,25 @@ module FormtasticBootstrap
   module Inputs
     module Base
       module Html
-
         include Formtastic::Inputs::Base::Html
 
         def form_control_input_html_options
-          orig_class = input_html_options[:class]
+          options = input_html_options.symbolize_keys
+          orig_class = options[:class]
           new_class = [orig_class, "form-control"].compact.join(" ")
-          input_html_options.merge(:class => new_class)
+          options.merge(:class => new_class)
         end
 
         def input_html_options
+          options = super.symbolize_keys
           if errors?
-            {
-              :class => "error"
-            }.merge(super)
+            { :class => "error" }.merge(options) do |key, oldval, newval|
+              key == :class ? [newval, oldval].compact.join(" ") : newval
+            end
           else
-            super
+            options
           end
         end
-
       end
     end
   end
